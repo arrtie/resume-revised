6f8ff4bc02f8dc771f87881a19ba438af2908172
Artie <artierosser@gmail.com>
2025-03-03 19:02:06 -0600

    reduced boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        content here
+      </main>
+      <footer>
+        {(new Date()).getFullYear()}
+      </footer>
+    </div>
+  );
+}
